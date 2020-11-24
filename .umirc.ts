1442fffba7fb00dba014785b1cfbcf7ef72c6451
koki <zghong@huanbaoyun.cloud>
2020-11-25 00:39:15 +0800

    feat: update icon

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   // more config: https://d.umijs.org/config
   title: '子宓的笔记',
+  logo: '/logo.jpeg',
+  favicon: '/favicon.ico',
   mode: 'site',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 });
